fc7d0b3028ffa2fff5ed417e926543b96f703487
bdeng <mixiaochao@foxmail.com>
2023-06-06 15:52:01 +0800

    Add files

---
+++ src/components/Exercise2-01.vue
@@ -0,0 +1,20 @@
+<template>
+  <input v-model="firstName" placeholder="First name" />
+  <input v-model="lastName" placeholder="Last name" />
+  <h3 class="output">{{ fullName }}</h3>
+</template>
+<script>
+export default{
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`
+    }
+  },
+
+}</script>
