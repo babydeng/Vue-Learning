6d26a5877145889f12242bf2ec8461b6c4756ca5
bdeng <mixiaochao@foxmail.com>
2023-06-06 20:34:37 +0800

    Add Exercise2-02.vue

---
+++ src/components/Exercise2-02.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="container">
+  <input type="number" v-model="incrementOne" />
+  <h3>Get input: {{ incrementOne }}</h3>
+  <h5>Set division: {{ divideByTwo }}</h5>
+
+</div>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      count: -1,
+      divideByTwo: 0,
+    }
+  },
+  computed: {
+    incrementOne: {
+    // getter
+    get(){
+        return this.count + 1
+      },
+    // setter
+    set(value){
+        this.count = value - 1
+        this.divideByTwo = value / 2
+      }
+    }  
+  },
+
+}</script>
